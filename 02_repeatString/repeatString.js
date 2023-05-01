be6b917c237b13a626cc4f7461e5139c8ad8f4e1
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2023-05-01 11:24:12 +0300

    create function t0 repeat word

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,23 @@
-const repeatString = function() {
+/*
+the function accept word and 
+use for loop to repeat the word using desired repetition number and  repeat the word desired times and 
+return repeated word.
+*/
 
-};
+const  repeatString = function(word,repetitionNumber){
+        if(repetitionNumber<0) return "ERROR";
+
+        let finalWord="";//variable to hold the repeated word
+
+        for (let i = 0; i < repetitionNumber; i++){
+            
+             finalWord += word;
+        }
+          
+        return finalWord;
+    }
+repeatString();
+
 
 // Do not edit below this line
 module.exports = repeatString;
