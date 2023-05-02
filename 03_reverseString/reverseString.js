220baac23212a7b9a36c04e3bfef87775336e313
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2023-05-02 13:29:23 +0300

    function to reverse strig

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,23 @@
-const reverseString = function() {
 
-};
 
+const reverseString = function(word) {
+    
+     // store letters in array form by using  split
+     let wordSlice = word.split("");
+
+     //we reverse the array and put it in sliceArray
+     let sliceArray = wordSlice.reverse();
+     
+    let finalWord = "";//to store the reversed string at end
+    
+     //we use for loop on slicearray element and store them in finalWord  variable as reverse string
+     for ( i = 0;i < sliceArray.length;i++){
+ 
+         finalWord = finalWord.concat(sliceArray[i]);
+     }
+     
+     return finalWord; 
+ 
+    }
 // Do not edit below this line
 module.exports = reverseString;
