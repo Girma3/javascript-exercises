267811ae27f67b2cb4bf62649cd71b50af09dd3c
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2023-05-08 11:57:16 +0300

    functiion created to check the year is leap year or not

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,18 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    //if the year is divisible by 100 and 400 it is leap year
+    if (year % 100 === 0 && year % 400 === 0){
+        return true;
+    }
+
+    // if the year is divisible by 4 but not by 100 it is leap year
+    else if (year % 4 ===0 && year % 100 !== 0){
+       return true;
+    }
+
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
