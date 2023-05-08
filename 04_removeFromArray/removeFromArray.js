0776f39583533045c639ddb2d2c2b5c19202786e
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2023-05-08 15:25:21 +0300

    function created to emove items from the array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,25 @@
-const removeFromArray = function() {
+const removeFromArray = function(myarray,...args) {
+    //loop through both both array and compare each element if equal value found we remove it
+    //...args is array we can accept multiple arguments in the form of array it is called spread method.
+
+    for(let i = 0;i < args.length; i++){
+        /*the second loop is count  backward  because when we remove item the index reshuffles 
+        *to deal with that we start from the last item
+        */
+
+        for(let j = myarray.length -1  ;j >= 0; j--){
+        //only remove using slice if items are same
+
+            if(myarray[j] === args[i]){
+               myarray.splice(j,1)
+            }
+
+        }
+    }
+   return myarray;
 
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
