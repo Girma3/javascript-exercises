037465b2854f4d38700b1110875c2efeed1489dd
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2023-06-19 18:08:54 +0300

    two function created to convert temperature

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,25 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+ //to convert fahrenheit to celsius 
+  
+  celsius = (fahrenheit - 32) * 5/9;
+
+//round it to one decimal to be more readable
+  result = Math.round(celsius * 10) / 10;
+
+return result
 };
 
-const convertToFahrenheit = function() {
-};
+const convertToFahrenheit = function(celsius) {
+  //to convert celsius to fahrenheit
+  fahrenheit = celsius * 9/5 + 32;
 
+  //round it to one decimal to be more readable
+  result =Math.round(fahrenheit * 10) / 10
+
+  return result
+ };
+
+//console.log(convertToFahrenheit(32))
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
