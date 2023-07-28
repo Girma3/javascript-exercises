5c2d5c88e9df96f4be8f2f78429d5f5c472a31ee
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2023-07-28 11:27:13 +0300

    function created to get all the titles as an array

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,21 @@
-const getTheTitles = function() {
-
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
+const getTheTitles = function(arrray) {
+let titles = [];
+for (let i = 0;i < arrray.length;i++){
+    let title = arrray[i].title
+    titles.push(title)
+}
+return titles
 };
-
+console.log(getTheTitles(books))
 // Do not edit below this line
 module.exports = getTheTitles;
