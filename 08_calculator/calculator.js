cffb0703f44f71932d142266e03d844c7ab09ab3
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2023-07-28 10:15:58 +0300

    function created to  do basic maths operations

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,28 +1,48 @@
-const add = function() {
+const add = function(a,b) {
+  let result = a + b;
+	return result
+};
+
+const subtract = function(c,d) {
+  let diffrence = c - d
+  return diffrence
 	
 };
 
-const subtract = function() {
-	
+const sum = function(array) {
+  let initialValue = 0
+ let result =  array.reduce((total,numbers)=>total+ numbers,initialValue);
+	return result
 };
 
-const sum = function() {
-	
+
+const multiply = function(array) {
+let result = array.reduce((total,numbers)=>total * numbers);
+return result
 };
 
-const multiply = function() {
-
+const power = function(base,exponent) {
+	let powerNumber = 1
+  for(i = 0;i < exponent;i++){
+    powerNumber = powerNumber * base
+  }
+  return powerNumber
 };
 
-const power = function() {
-	
-};
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	a = 1;//to return 1 for  0 factorial
+  
+  // the loop  start from 1
+  for(let i = 1; i <= num;i++){
+      
+      a *= i
+   }
+return a
 };
 
 // Do not edit below this line
+
 module.exports = {
   add,
   subtract,
