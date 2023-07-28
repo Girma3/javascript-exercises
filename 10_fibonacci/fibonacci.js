d338394231eaa60cdd8f4f75f66bfaf5aaa2ac38
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2023-07-28 11:11:44 +0300

    function created to return fibonacci sequece member position

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(userNum) {
+    // check if the user enter negative number and convert if string enterd to number
+let num = Number(userNum);
+if(userNum < 0) return "OOPS"
 
+    
+    let a = 0;
+    let b = 1;
+    let c = num
+    // use for loop as we iterate each value interchange place
+    for(let i = 1;i<num;i++ ){
+        c = a + b;
+        a = b;
+        b = c;
+        
+    }
+return c
 };
 
 // Do not edit below this line
