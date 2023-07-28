f86ffbce64a84ea712be2aa10405e0f6dbd11852
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2023-07-28 10:46:46 +0300

    function created to check if a word is palindrome or not

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,17 @@
-const palindromes = function () {
+const palindromes = function (userword) {
+//remove punctuation with regular expression and replace it with space
+let newformat = userword.replace(/[\s ! ,\.]/g,"").toLowerCase();
+
+//turn the word to  array of letters
+let userwordArray = newformat.split("");
+
+// we convert the  word  into an array then reverse it backward 
+let reverseUserWord = [...newformat].reverse();
+
+//check backward  and forward string are the same  if it  is true it is palindrome word
+let result = userwordArray.toString() === reverseUserWord.toString();
+
+return result;
 
 };
 
